fbf97eea6a3c656a23f039fd684485847b63e0fa
SarahSchill <sschill@uncc.edu>
2017-11-06 21:25:52 +0000

    first commit

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -1,4 +1,12 @@
 class PlayerController < ApplicationController
+    def index
+
+    end
+    
     def new
+    end
+    
+    def create
+        render plain:params[:player].inspect
     end
 end
