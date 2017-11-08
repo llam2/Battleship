9b323bef4c1e4412ff0a23c09fb6ac94052e14c1
Leon <llam2@uncc.edu>
2017-11-08 00:53:23 +0000

    Added Audio to Initial Front End

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,15 @@
+function playclip() {
+
+var audio = document.getElementsByTagName("audio")[1];
+audio.volume=.5;
+audio.play();
+
+}
+
+function playclip2() {
+
+var audio = document.getElementsByTagName("audio")[2];
+audio.volume=.5;
+audio.play();
+
+}
