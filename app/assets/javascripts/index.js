edb19dfa4321d6d4035683e8facabb069a3816ef
Leon <llam2@uncc.edu>
2017-11-08 03:48:20 +0000

    Functioning Nav Buttons

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,3 +1,7 @@
+window.onload = function() {
+ 
+};
+
 function playclip() {
 
 var audio = document.getElementsByTagName("audio")[1];
@@ -6,10 +10,47 @@ audio.play();
 
 }
 
-function playclip2() {
+function onePlayer() {
 
 var audio = document.getElementsByTagName("audio")[2];
+var y = document.getElementById("options");
+y.style.display = "none";
+var z = document.getElementById("scores");
+z.style.display = "none";
+var x = document.getElementById("wholeGrid");
+x.style.display = "block";
 audio.volume=.5;
 audio.play();
 
 }
+
+function options() {
+
+var audio = document.getElementsByTagName("audio")[2];
+var x = document.getElementById("wholeGrid");
+x.style.display = "none";
+var z = document.getElementById("scores");
+z.style.display = "none";
+var y = document.getElementById("options");
+y.style.display = "block";
+audio.volume=.5;
+audio.play();
+
+}
+
+function scores() {
+
+var audio = document.getElementsByTagName("audio")[2];
+var x = document.getElementById("wholeGrid");
+x.style.display = "none";
+var y = document.getElementById("options");
+y.style.display = "none";
+var z = document.getElementById("scores");
+z.style.display = "block";
+audio.volume=.5;
+audio.play();
+
+}
+
+
+
