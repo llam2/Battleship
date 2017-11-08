050b6a6c333c44850217b8f6f9d83edb213d10f0
Leon <llam2@uncc.edu>
2017-11-08 10:10:07 +0000

    Added Options Checkbox and Changed Grid

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,54 +1,84 @@
 window.onload = function() {
- 
+	var audio = document.getElementsByTagName("audio")[1];
+	var audio2 = document.getElementsByTagName("audio")[2];
+	audio.volume = .5;
+	audio2.volume = .5;
 };
+
 
 function playclip() {
 
-var audio = document.getElementsByTagName("audio")[1];
-audio.volume=.5;
-audio.play();
+	var audio = document.getElementsByTagName("audio")[1];
+	audio.play();
 
 }
 
 function onePlayer() {
 
-var audio = document.getElementsByTagName("audio")[2];
-var y = document.getElementById("options");
-y.style.display = "none";
-var z = document.getElementById("scores");
-z.style.display = "none";
-var x = document.getElementById("wholeGrid");
-x.style.display = "block";
-audio.volume=.5;
-audio.play();
+	var audio = document.getElementsByTagName("audio")[2];
+	var y = document.getElementById("options");
+	y.style.display = "none";
+	var z = document.getElementById("scores");
+	z.style.display = "none";
+	var x = document.getElementById("wholeGrid");
+	x.style.display = "block";
+	audio.play();
 
 }
 
 function options() {
 
-var audio = document.getElementsByTagName("audio")[2];
-var x = document.getElementById("wholeGrid");
-x.style.display = "none";
-var z = document.getElementById("scores");
-z.style.display = "none";
-var y = document.getElementById("options");
-y.style.display = "block";
-audio.volume=.5;
-audio.play();
+	var audio = document.getElementsByTagName("audio")[2];
+	var x = document.getElementById("wholeGrid");
+	x.style.display = "none";
+	var z = document.getElementById("scores");
+	z.style.display = "none";
+	var y = document.getElementById("options");
+	y.style.display = "block";
+	audio.play();
 
 }
 
 function scores() {
 
-var audio = document.getElementsByTagName("audio")[2];
-var x = document.getElementById("wholeGrid");
-x.style.display = "none";
-var y = document.getElementById("options");
-y.style.display = "none";
-var z = document.getElementById("scores");
-z.style.display = "block";
-audio.volume=.5;
-audio.play();
+	var audio = document.getElementsByTagName("audio")[2];
+	var x = document.getElementById("wholeGrid");
+	x.style.display = "none";
+	var y = document.getElementById("options");
+	y.style.display = "none";
+	var z = document.getElementById("scores");
+	z.style.display = "block";
+	audio.play();
+
+}
+
+function musicToggle(cb) {
+
+	/*alert("Clicked, new value = " + cb.checked);*/
+	var audio = document.getElementById("backgroundMusic");
+
+	if (cb.checked == true){
+		audio.volume = 0;
+	}
+	else if (cb.checked == false){
+		audio.volume = 1;
+	}
+
+}
+
+function soundFXToggle(cb) {
+
+	var audio = document.getElementsByTagName("audio")[1];
+	var audio2 = document.getElementsByTagName("audio")[2];
+
+	if (cb.checked == true){
+		audio.volume = 0;
+		audio2.volume = 0;
+	}
+	else if (cb.checked == false){
+		audio.volume = .5;
+		audio2.volume = .5;
+	}
 
 }
 
