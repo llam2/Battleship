4ba338fadc69c15924c71d6a97c65360a9ac7340
Kolby <kviera1@uncc.edu>
2017-11-15 22:34:26 +0000

    Add CRUD to player controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,25 @@
 class PlayersController < ApplicationController
+    
+    def new
+        @player = Player.new
+    end
+    
+    def show
+        @player = Player.find(params[:id])
+    end
+    
+    def create
+        @player = Player.all
+        
+         if @player.save
+            
+            redirect_to @player
+        else
+            render 'new'
+        end
+end
+
+def destroy
+    @player = Player.find(params[:id])
+    @player.destroy
 end
--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,2 +1,6 @@
 class ScoresController < ApplicationController
+    
+    def new
+    @score = Scores.new
+end
 end
